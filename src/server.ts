b934aeff15b31a0e1417fde02d73febe6414bb48
Ayush Rameja <ayushrwork@gmail.com>
2024-10-06 17:09:04 +0530

    Removed bun code and added node and express code

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import app from "./routes";
-import { serve } from "bun";
-
-serve({
-  fetch: app.fetch,
-  port: 8080,
-});
-
-console.log(`Portfolio backend is runnning on http://localhost:8080`);
