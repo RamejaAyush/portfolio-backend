7e76d9f6f5dd0f1cafbec49a8a264e2d9f425ffc
Ayush Rameja <ayushrwork@gmail.com>
2024-10-13 11:50:39 +0530

    Removed hono code

--- src/interface/IRequest.ts
+++
@@ -1,4 +0,0 @@
-export interface IRequest {
-  name: string;
-  content: string;
-}
--- src/interface/env.ts
+++
@@ -1,7 +0,0 @@
-export type IENV = {
-  MY_NAME: string;
-  EMAIL: string;
-  SENDTO: string;
-  PASSWORD: string;
-  SENDGRID_API: string;
-};
