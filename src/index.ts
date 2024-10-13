13cea8fef2c811805154ecd559ee55a8529259b7
Ayush Rameja <ayushrwork@gmail.com>
2024-10-13 11:49:06 +0530

    Added CORS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { IENV } from "./interface/env";
 import smtpRouter from "./routes/smtp.route";
+import { limiter } from "./middleware/limitter.middleware";
 
 const app = new Hono<{ Bindings: IENV }>();
+
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173", "http://www.ayushrameja.com"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    maxAge: 86400,
+  })
+);
 
 app.use(logger());
 
